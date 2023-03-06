4fe0168e54d639d1c85b1db317ce0b0b6e044eb2
asus <111202113694@mhs.dinus.ac.id>
2023-03-06 23:22:51 +0700

    first commit

---
+++ SegitigaSikuSiku/LuasSegitigaSiku.java
@@ -0,0 +1,13 @@
+package SegitigaSikuSiku;
+
+public class LuasSegitigaSiku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double l,a=6,t=8;
+		l= 0.5 * a * t;
+		System.out.print("Luas dari Segitiga Siku-Siku yang mempunyai alas 6cm dan tinggi 8cm yaitu : "
+				+l+"cm");
+	}
+
+}
